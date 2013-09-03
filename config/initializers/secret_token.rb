642c002a10dad31eabb9c07ec388bdc8439e9b33
rootuj <rootu.joshi@gmail.com>
2013-09-02 20:58:57 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Elementalweb::Application.config.secret_key_base = 'a22224dbbef3d05c60b355ba4a395a1b436c050d7ddfc6037717e46da9633c820067064c4732614c269e33c25555cfff8f95a21b1975c5bce0d7c70aa9b8409c'
